4b1e288170c7611719e38b2186c30440bc703e2f
Luca Mazzucco <115407939+LM1997610@users.noreply.github.com>
2023-03-09 21:04:10 +0100

    Update rebus_bot.py

--- rebus_bot.py
+++ rebus_bot.py
@@ -78,3 +78,23 @@ def callback_query(call):
         bot.answer_callback_query(call.id, "'Error 13: Rebus not defined'")
 
 bot.infinity_polling()
+
+
+'''
+@server.route('/' + bot_token , methods=['POST'])
+def getMessage():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://BradGalben.pythonanywhere.com/' + bot_token )
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))'''
